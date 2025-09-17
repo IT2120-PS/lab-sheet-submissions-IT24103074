32d2194a3d39d6b4a85b47be7bad32eb4e4272ec
IT24103074 <it24103074@my.sliit.lk>
2025-09-17 10:31:55 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24103074.R
@@ -0,0 +1,12 @@
+setwd("C:\\Users\\Senuhi\\OneDrive\\Desktop\\IT24103074")
+#question 1
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+
+#question 2
+pexp(2, rate = 1/3)
+
+#question 3
+#part 1
+1 - pnorm(130, mean = 100, sd = 15)
+#part 2
+qnorm(0.95, mean = 100, sd = 15)
